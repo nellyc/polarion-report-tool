271e33e172d1704f6a1850357ffe6ace836b1ccb
Nelly Credi <ncredi@redhat.com>
2020-06-11 19:59:50 +0300

    Work to make this tool more user friendly
    split some of the data from polarion report into run_report
    created example xls

---
+++ run_report.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import datetime 
+
+from pylarion.test_run import TestRun  
+from pylarion.work_item import TestCase, Requirement  
+from pylarion.document import Document  
+
+from config import *
+from helpers import *
+
+# Test runs execution status per team
+g = gapi.GoogleSpreadSheetAPI(SPREADSHEET_NAME, "Polarion test run")
+
+# Test runs execution status per plan and run properties
+row_number = 3
+for curr in field_dicts_list:
+    g.update_sheet(row_number,5, find_number_of_TCs_per_status_and_run_fields(run_fields_dict=field_dicts[curr], status='*'))
+    g.update_sheet(row_number,6, find_number_of_TCs_per_status_and_run_fields(run_fields_dict=field_dicts[curr], status='passed'))
+    g.update_sheet(row_number,7, find_number_of_TCs_per_status_and_run_fields(run_fields_dict=field_dicts[curr], status='@null'))
+    row_number += 1
